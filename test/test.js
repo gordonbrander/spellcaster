8f2282786cfc4352742244e3606a010c90e103a6
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-01-10 08:07:29 -0700

    Update tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,127 @@
 import { describe, it } from "mocha"
-import { strict as assert } from "assert"
+import { strict as assert, fail } from "assert"
 
-describe('test', () => {
-    it('should run', () => {
-        assert(true)
+import {
+  withTracking,
+  getTracked,
+  signal,
+  effect,
+  computed
+} from "../tendril.js"
+
+describe('withTracking', () => {
+  it('it executes the body function immediately and returns the value', () => {
+    const onChange = () => {}
+
+    const didExecute = withTracking(
+        onChange,
+        () => true
+    )
+    assert(didExecute)
+  })
+
+  it('sets the dependency for the tracking scope', done => {
+    const onChange = () => {}
+
+    withTracking(
+      onChange,
+      () => {
+        const dependency = getTracked()
+        assert(dependency === onChange)
+        done()
+      }
+    )
+  })
+})
+
+describe('getTracked', () => {
+  it('is undefined outside of tracking scopes', () => {
+    const dependency = getTracked()
+
+    assert(dependency == null)
+  })
+})
+
+describe('signal getter', () => {
+  it('returns value', () => {
+    const [state, _] = signal(0)
+    assert(state() === 0)
+  })
+
+  it('triggers withTracking transaction callback on change, when read within reactive scope', done => {
+    const [state, setState] = signal(0)
+
+    withTracking(
+      () => {
+        // Done should be called during next transaction
+        done()
+      },
+      () => {
+        // Access state so that it is tracked
+        state()
+      }
+    )
+
+    setState(10)
+  })
+})
+
+describe('signal setter', () => {
+  it('sets value immediately', () => {
+    const [state, setState] = signal(0)
+    setState(10)
+    assert(state() === 10)
+  })
+})
+
+
+describe('effect', () => {
+  it('executes once on initialization', done => {
+    effect(done)
+  })
+
+  it('reacts to signals once per microtask, batching multiple updates', done => {
+    const [a, setA] = signal(1)
+    const [b, setB] = signal(1)
+
+
+    let count = 0
+    effect(() => {
+      count++
+      if (count > 1) {
+        fail('Effect fired too many times')
+      } else if (count === 1) {
+        done()
+      }
     })
+
+    setA(10)
+    setB(10)
+  })
+})
+
+describe('computed', () => {
+  it('computes immediately and returns value', () => {
+    const [a, setA] = signal(1)
+    const [b, setB] = signal(1)
+    const sum = computed(() => a() + b())
+
+    assert(sum() === 2)
+  })
+
+  it('reacts to signal changes by recomputing on next microtask', async () => {
+    const [a, setA] = signal(1)
+    const [b, setB] = signal(1)
+    const sum = computed(() => a() + b())
+
+    assert(sum() === 2)
+
+    setA(10)
+    setB(10)
+
+    // Recomputes are batched on next microtask, so await next microtask
+    await Promise.resolve()
+
+    assert(sum() === 20)
+  })
 })
