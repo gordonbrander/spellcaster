df9364bd411fd04bf7d150b12efff36bb0271dcd
Gordon Brander <gordonbrander@users.noreply.github.com>
2024-01-24 08:48:31 -0500

    Refine hyperscript types
    ...and add typescript examples for counter and todos.

---
+++ examples/typescript-counter/main.ts
@@ -0,0 +1,23 @@
+import {signal} from '../../spellcaster.js'
+import {tags, text} from '../../hyperscript.js'
+const {div, button} = tags
+
+const Counter = (): HTMLElement => {
+  const [count, setCount] = signal(0)
+
+  return div(
+    {className: 'counter'},
+    [
+      div({className: 'counter-text'}, text(count)),
+      button(
+        {
+          className: 'counter-button',
+          onclick: () => setCount(count() + 1)
+        },
+        text('Increment')
+      )
+    ]
+  )
+}
+
+document.body.append(Counter())
---
+++ examples/typescript-todos/main.ts
@@ -0,0 +1,210 @@
+import {
+  store,
+  computed,
+  next,
+  unknown,
+  Transaction
+} from '../../spellcaster.js'
+
+import {
+  tags,
+  repeat,
+  text,
+  cid,
+  index
+} from '../../hyperscript.js'
+
+const {div, button, input} = tags
+
+// Messages
+
+type Msg = UpdateInputMsg | SubmitInputMsg | CompleteMsg
+
+type UpdateInputMsg = {
+  type: 'updateInput'
+  value: string
+}
+
+const updateInputMsg = (value: string): UpdateInputMsg => ({
+  type: 'updateInput',
+  value
+})
+
+type SubmitInputMsg = {
+  type: 'submitInput'
+  value: string
+}
+
+const submitInputMsg = (value: string): SubmitInputMsg => ({
+  type: 'submitInput',
+  value
+})
+
+type CompleteMsg = {
+  type: 'complete'
+  id: string
+  isComplete: boolean
+}
+
+const completeMsg = (id: string, isComplete: boolean): CompleteMsg => ({
+  type: 'complete',
+  id,
+  isComplete
+})
+
+// Models and views
+
+type TodoModel = {
+  id: string
+  isComplete: boolean
+  text: string
+}
+
+const TodoModel = ({
+  id=cid(),
+  isComplete=false,
+  text=''
+}): TodoModel => ({
+  id,
+  isComplete,
+  text
+})
+
+const Todo = (
+  todo: () => TodoModel,
+  send: (msg: Msg) => void
+) => div(
+  {className: 'todo'},
+  [
+    div(
+      {className: 'todo-text'},
+      text(() => todo().text)
+    ),
+    button(
+      {
+        className: 'button-done',
+        onclick: () => send(completeMsg(todo().id, true))
+      },
+      text('Done')
+    )
+  ]
+)
+
+type InputModel = {
+  text: string
+}
+
+const InputModel = ({text=''}): InputModel => ({text})
+
+const TodoInput = (
+  state: () => InputModel,
+  send: (msg: Msg) => void
+) => input(
+  () => ({
+    value: state().text,
+    placeholder: 'Enter todo...',
+    oninput: event => send(updateInputMsg(event.target.value)),
+    onkeyup: event => {
+      if (event.key === 'Enter') {
+        send(submitInputMsg(event.target.value))
+      }
+    },
+    type: 'text',
+    className: 'todo-input'
+  })
+)
+
+type AppModel = {
+  input: InputModel
+  todos: Map<string, TodoModel>
+}
+
+const AppModel = ({
+  input=InputModel({}),
+  todos=new Map<string, TodoModel>()
+}): AppModel => ({
+  input,
+  todos
+})
+
+const App = (
+  state: () => AppModel,
+  send: (msg: Msg) => void
+): HTMLElement => div(
+  {className: 'app'},
+  [
+    TodoInput(
+      computed(() => state().input),
+      send
+    ),
+    div(
+      {className: 'todos'},
+      repeat(Todo, computed(() => state().todos), send)
+    )
+  ]
+)
+
+const init = (): Transaction<AppModel, Msg> => next(
+  AppModel({})
+)
+
+const update = (
+  state: AppModel,
+  msg: Msg
+): Transaction<AppModel, Msg> => {
+  switch (msg.type) {
+  case 'updateInput':
+    return updateInput(state, msg.value)
+  case 'submitInput':
+    return submitInput(state, msg.value)
+  case 'complete':
+    return complete(state, msg.id)
+  default:
+    return unknown(state, msg)
+  }
+}
+
+const updateInput = (
+  state: AppModel,
+  text: string
+): Transaction<AppModel, Msg> => next({
+  ...state,
+  input: InputModel({text})
+})
+
+const submitInput = (
+  state: AppModel,
+  text: string
+): Transaction<AppModel, Msg> => next({
+  ...state,
+  input: InputModel({text: ''}),
+  todos: index([
+    ...state.todos.values(),
+    TodoModel({text})
+  ])
+})
+
+const complete = (
+  state: AppModel,
+  id: string
+): Transaction<AppModel, Msg> => {
+  if (!state.todos.has(id)) {
+    console.log("No item for ID. Doing nothing.", id)
+    return next(state)
+  }
+  const todos = new Map(state.todos)
+  todos.delete(id)
+  return next({
+    ...state,
+    todos
+  })
+}
+
+const [state, send] = store({
+  init,
+  update,
+  debug: true
+})
+
+const appEl = App(state, send)
+document.body.append(appEl)
